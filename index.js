219cd6458faf4ed556981fcf1af992720e5941d7
jonschlinkert <jon.schlinkert@sellside.com>
2016-08-29 22:28:52 -0400

    first release

--- index.js
+++ index.js
@@ -2,50 +2,48 @@
 
 var util = require('util');
 var Paginator = require('terminal-paginator');
-var BasePrompt = require('enquirer-prompt');
-var cursor = require('cli-cursor');
+var Prompt = require('enquirer-prompt');
 var radio = require('radio-symbol');
+var cursor = require('cli-cursor');
 var log = require('log-utils');
 
 /**
- * Checkbox prompt
+ * Radio prompt
  */
 
-function Prompt(/*question, rl, answers*/) {
-  BasePrompt.apply(this, arguments);
-  if (!this.question.choices) {
+function Radio(/*question, answers, rl*/) {
+  Prompt.apply(this, arguments);
+  if (!this.choices) {
     throw new Error('expected "options.choices" to be an array');
   }
   this.setDefault();
-  this.pointer = 0;
+  this.pointer = this.question.choices.getIndex(this.question.default) || 0;
   this.question.default = null;
   this.paginator = new Paginator();
 }
 
 /**
- * Inherit BasePrompt
+ * Inherit Prompt
  */
 
-util.inherits(Prompt, BasePrompt);
+util.inherits(Radio, Prompt);
 
 /**
  * Start the prompt session
  * @param  {Function} `cb` Callback when prompt is finished
- * @return {Object} Returns the `Prompt` instance
+ * @return {Object} Returns the `Radio` instance
  */
 
-Prompt.prototype.ask = function(cb) {
+Radio.prototype.ask = function(cb) {
   this.callback = cb;
-  var self = this;
-
-  this.ui.once('line', function(e) {
-    self.onSubmit({value: self.getCurrentValue(e)});
-  });
 
   this.ui.on('up', this.onUpKey.bind(this));
   this.ui.on('down', this.onDownKey.bind(this));
   this.ui.on('space', this.onSpaceKey.bind(this));
   this.ui.on('number', this.onNumberKey.bind(this));
+
+  this.ui.once('line', this.onSubmit.bind(this));
+  this.ui.once('error', this.onError.bind(this));
 
   // Init the prompt
   cursor.hide();
@@ -57,7 +55,7 @@ Prompt.prototype.ask = function(cb) {
  * Render the prompt to the terminal
  */
 
-Prompt.prototype.render = function(error) {
+Radio.prototype.render = function(error) {
   // Render question
   var message = this.message;
   var append = '';
@@ -70,8 +68,8 @@ Prompt.prototype.render = function(error) {
   if (this.status === 'answered') {
     message += log.cyan(this.selection.join(', '));
   } else {
-    var choicesStr = renderChoices(this.question.choices, this.pointer);
-    var idx = this.question.choices.indexOf(this.question.choices.getChoice(this.pointer));
+    var choicesStr = renderChoices(this.choices, this.pointer, this.options);
+    var idx = this.choices.indexOf(this.choices.getChoice(this.pointer));
     message += '\n' + this.paginator.paginate(choicesStr, idx, this.question.pageSize);
   }
 
@@ -81,64 +79,80 @@ Prompt.prototype.render = function(error) {
   this.ui.render(message, append);
 };
 
-Prompt.prototype.onUpKey = function() {
-  var len = this.question.choices.realLength;
-  this.pointer = (this.pointer > 0) ? this.pointer - 1 : len - 1;
-  this.render();
-};
+/**
+ * When an error event is emitted
+ */
 
-Prompt.prototype.onDownKey = function() {
-  var len = this.question.choices.realLength;
-  this.pointer = (this.pointer < len - 1) ? this.pointer + 1 : 0;
-  this.render();
-};
-
-Prompt.prototype.onNumberKey = function(input) {
-  if (input <= this.question.choices.realLength) {
-    this.pointer = input - 1;
-    this.toggleChoice(this.pointer);
-  }
-  this.render();
-};
-
-Prompt.prototype.onSpaceKey = function() {
-  this.spaceKeyPressed = true;
-  this.toggleChoice(this.pointer);
-  this.render();
-};
-
-Prompt.prototype.toggleChoice = function(index) {
-  var checked = this.question.choices.getChoice(index).checked;
-  this.question.choices.getChoice(index).checked = !checked;
+Radio.prototype.onError = function(answer) {
+  this.render(answer.isValid);
 };
 
 /**
- * When the user presses the `enter` key
+ * When user presses the `enter` key
  */
 
-Prompt.prototype.onSubmit = function(answer) {
-  this.answer = answer;
+Radio.prototype.onSubmit = function(line) {
+  this.answer = this.getSelectedValues(line);
   this.status = 'answered';
   this.render();
   this.ui.write();
   cursor.show();
-  this.callback(answer.value);
+  this.callback(this.answer);
 };
 
-Prompt.prototype.onError = function(answer) {
-  this.render(answer.isValid);
+/**
+ * When user presses the `up` key
+ */
+
+Radio.prototype.onUpKey = function() {
+  var len = this.choices.realLength;
+  this.pointer = (this.pointer > 0) ? this.pointer - 1 : len - 1;
+  this.render();
+};
+
+/**
+ * When user presses the `down` key
+ */
+
+Radio.prototype.onDownKey = function() {
+  var len = this.choices.realLength;
+  this.pointer = (this.pointer < len - 1) ? this.pointer + 1 : 0;
+  this.render();
+};
+
+/**
+ * When user presses a number key
+ */
+
+Radio.prototype.onNumberKey = function(event) {
+  var num = Number(event.value);
+  if (num <= this.choices.realLength) {
+    this.pointer = num - 1;
+    this.question.toggleChoices(this.pointer);
+  }
+  this.render();
+};
+
+/**
+ * When user presses the `space` bar
+ */
+
+Radio.prototype.onSpaceKey = function() {
+  this.spaceKeyPressed = true;
+  this.question.toggleChoices(this.pointer);
+  this.render();
 };
 
 /**
  * Set the default value to use
  */
 
-Prompt.prototype.setDefault = function() {
-  if (Array.isArray(this.question.default)) {
-    var len = this.question.choices.length;
+Radio.prototype.setDefault = function() {
+  if (Array.isArray(this.question.choices)) {
+    var len = this.choices.length;
     var idx = -1;
     while (++idx < len) {
-      var choice = this.question.choices[idx];
+      var choice = this.choices[idx];
       if (contains(this.question.default, choice.value)) {
         choice.checked = true;
       }
@@ -147,73 +161,96 @@ Prompt.prototype.setDefault = function() {
 };
 
 /**
- * Get the currently defined value
+ * Get the currently selected value
  */
 
-Prompt.prototype.getCurrentValue = function() {
-  var choices = this.question.choices.filter(function(choice) {
+Radio.prototype.getSelectedValues = function() {
+  var choices = this.choices.filter(function(choice) {
     return Boolean(choice.checked) && !choice.disabled;
   });
-
   this.selection = choices.map(function(choice) {
     return choice.short;
   });
-
   return choices.map(function(choice) {
     return choice.value;
   });
 };
 
 /**
- * Function for rendering checkbox choices
- * @param {Array} `choices` Array of choice objects
- * @param {Number} `pointer` Position of the pointer
- * @return {String} Rendered content
+ * Render the checkbox based on state.
+ * @param  {Boolean} `checked` If enabled, adds an X to the checkbox
+ * @return {String} returns the rendered radio-button string
  */
 
-function renderChoices(choices, pointer) {
+function renderChoice(choice) {
+  return choice.checked ? log.green(radio.on) : radio.off;
+}
+
+/**
+ * Function for rendering checkbox choices
+ * @param  {Number} pointer Position of the pointer
+ * @return {String} Rendered choices string
+ */
+
+function renderChoices(choices, pointer, options) {
+  var offset = 0;
   var output = '';
-  var separatorOffset = 0;
 
   choices.forEach(function(choice, i) {
     if (choice.type === 'separator') {
-      separatorOffset++;
-      output += ' ' + choice + '\n';
-      return;
-    }
+      offset++;
+      output += inactive(choice);
 
-    if (choice.disabled) {
-      separatorOffset++;
-      output += ' - ' + choice.name;
-      output += ' (' + (typeof choice.disabled === 'string' ? choice.disabled : 'Disabled') + ')';
+    } else if (choice.disabled) {
+      offset++;
+      output += disabled(choice);
+
+    } else if (i - offset === pointer) {
+      output += active(choice, renderChoice(choice), options);
+
     } else {
-      var isSelected = (i - separatorOffset === pointer);
-      output += isSelected ? log.cyan('❯') : ' ';
-      output += getCheckbox(choice.checked) + ' ' + choice.name;
+      output += inactive(choice, renderChoice(choice));
     }
-
-    output += '\n';
+    output += ' \n';
   });
 
   return output.replace(/\n$/, '');
 }
 
 /**
- * Get the checkbox
- * @param  {Boolean} checked - add a X or not to the checkbox
- * @return {String} Composited checkbox string
+ * Utils
  */
 
-function getCheckbox(checked) {
-  return checked ? log.green(radio.on) : radio.off;
+function disabled(choice) {
+  var symbol = process.platform === 'win32' ? ' (×) ' : ' ⓧ ';
+  return log.dim(symbol + choice.name + ' (' + (choice.disabled || 'Disabled') + ')');
 }
 
-/**
- * Cast `val` to an array.
- */
+function inactive(choice, prefix) {
+  return ' ' + (prefix || '') + ' ' + choice.name;
+}
 
-function arrayify(val) {
-  return val ? (Array.isArray(val) ? val : [val]) : [];
+function active(choice, prefix, options) {
+  if (prefix && typeof prefix === 'object') {
+    options = prefix;
+    prefix = null;
+  }
+  return log.cyan(pointer(options) + (prefix || '') + ' ' + choice.name);
+}
+
+function pointer(options) {
+  if (options && typeof options.pointer === 'string') {
+    return options.pointer.trim() + ' ';
+  }
+  switch(process.platform) {
+    case 'win32':
+      return '>';
+    case 'linux':
+      return '‣';
+    default: {
+      return '❯';
+    }
+  }
 }
 
 /**
@@ -225,7 +262,15 @@ function contains(val, ele) {
 }
 
 /**
+ * Cast `val` to an array.
+ */
+
+function arrayify(val) {
+  return val ? (Array.isArray(val) ? val : [val]) : [];
+}
+
+/**
  * Module exports
  */
 
-module.exports = Prompt;
+module.exports = Radio;
